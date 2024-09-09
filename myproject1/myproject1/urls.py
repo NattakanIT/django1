7453b6c671f7756314605682357b49269a45ec18
maprang <6629011016@cdti.ac.th>
2024-09-09 16:23:10 +0700

    update

--- myproject1/myproject1/urls.py
+++ myproject1/myproject1/urls.py
@@ -20,5 +20,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('member.urls')),
+    path('', include('about.urls')),
+    path('', include('contact.urls')),
+    path('', include('home.urls')),
     path('admin/', admin.site.urls),
 ]
